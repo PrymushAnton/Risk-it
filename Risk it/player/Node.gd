0c782aa69397e7156e722934c617dc26d8fdc485
Илья Булкин <73954594+IliaBulkin@users.noreply.github.com>
2024-03-03 11:57:25 +0200

    added enemy and his running to the player

--- Risk it/player/Node.gd
+++ Risk it/player/Node.gd
@@ -2,18 +2,13 @@ extends Node
 
 @onready var pause_menu = $"../Control"
 
-
-
 var pause: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-
+func _process(_delta):
 	if pause == true:
 		get_tree().paused = true
 		pause_menu.show()
@@ -21,10 +16,8 @@ func _process(delta):
 		get_tree().paused = false
 		pause_menu.hide()
 
-
 func _on_pause_pressed():
 	pause = !pause
-
 
 func _on_close_pressed():
 	pause = !pause
--- Risk it/player/player.gd
+++ Risk it/player/player.gd
@@ -8,38 +8,58 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var animation_player
-
+var switch
+var go_to_player = false
 func _ready():
 	animation_player = get_node("AnimationPlayer")
+	
 
-func _physics_process(delta):
+func _process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle jump.
-
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	
 	if direction == 1:
 		$AnimatedSprite2D.flip_h = false
+		switch = true
 	elif direction == -1:
 		$AnimatedSprite2D.flip_h = true
-		
+		switch = false
+
 	if direction:
 		velocity.x = direction * SPEED
-		if is_on_floor():
+		if is_on_floor() and not Input.is_mouse_button_pressed(1):
 			animation_player.play('run')
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if is_on_floor():
+		if is_on_floor() and not Input.is_mouse_button_pressed(1):
 			animation_player.play('idle')
 	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		animation_player.play('jump')
-
+	
+	if switch == false:
+		$Area2D/CollisionShape2DRight.disabled = true
+		if Input.is_mouse_button_pressed(1):
+			$Area2D/CollisionShape2DLeft.disabled = false
+			animation_player.play('attack')
+		else:
+			$Area2D/CollisionShape2DLeft.disabled = true
+	else:
+		$Area2D/CollisionShape2DLeft.disabled = true
+		if Input.is_mouse_button_pressed(1):
+			$Area2D/CollisionShape2DRight.disabled = false
+			animation_player.play('attack')
+		else:
+			$Area2D/CollisionShape2DRight.disabled = true
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group('Hit'):
+		#body.take_damage()
+	#else:
+		#pass # Replace with function body.
+		body.a(body)
